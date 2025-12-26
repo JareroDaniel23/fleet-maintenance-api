5aca33daf8456b540c96b82987ffad8f85a9138e
Daniel Jarero <jarerodaniel323@gmail.com>
2025-12-26 11:23:42 -0600

    correcting the system

--- src/main/java/com/devsystem/erp/controllers/WashingServiceController.java
+++ src/main/java/com/devsystem/erp/controllers/WashingServiceController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/washing-service")
+@RequestMapping("/api/washing-services")
 @CrossOrigin(origins = "*")
 public class WashingServiceController {
 
