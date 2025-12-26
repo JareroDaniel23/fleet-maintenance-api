d3202f7664fee0f245c758a14597875284b1fca5
Daniel Jarero <jarerodaniel323@gmail.com>
2025-12-26 11:19:55 -0600

    Created controller for washing service! 💧

---
+++ src/main/java/com/devsystem/erp/controllers/WashingServiceController.java
@@ -0,0 +1,28 @@
+package com.devsystem.erp.controllers;
+
+import com.devsystem.erp.models.WashingService;
+import com.devsystem.erp.repositories.WashingServiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/washing-service")
+@CrossOrigin(origins = "*")
+public class WashingServiceController {
+
+@Autowired
+    private WashingServiceRepository washingServiceRepository;
+
+@GetMapping
+    public List<WashingService> getAllService() {
+    return washingServiceRepository.findAll();
+}
+
+@PostMapping
+    public WashingService createService(@RequestBody WashingService newService){
+    return washingServiceRepository.save(newService);
+}
+
+}
